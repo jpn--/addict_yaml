ab11e39c5cf1ebafc8413d639abfbafa791b812e
Jeff Newman <jeff@newman.me>
2018-10-16 13:28:19 -0500

    Bump version: 0.2.7 → 0.2.8

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='addict_yaml',
-    version='0.2.7',
+    version='0.2.8',
 
     description='Yaml-friendly attribute dictionaries',
     long_description=long_description,
