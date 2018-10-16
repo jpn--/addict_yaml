423a7dbccc0d60de9a9449f5a692f29f8f7a28df
Jeff Newman <jeff@newman.me>
2018-10-16 12:18:22 -0500

    Bump version: 0.2.5 → 0.2.6

--- how_to_update.sh
+++ how_to_update.sh
@@ -4,10 +4,10 @@ conda build ./conda --output-folder conda_builds/ -c jpn --python 3.6
 
 conda build ./conda --output-folder conda_builds/ -c jpn --python 3.7
 
-anaconda upload conda_builds/osx-64/addict_yaml-0.2.5-*.tar.bz2
+anaconda upload conda_builds/osx-64/addict_yaml-0.2.6-*.tar.bz2
 
-conda convert --platform win-64 conda_builds/osx-64/addict_yaml-0.2.5-*.tar.bz2 -o conda_builds/
-anaconda upload conda_builds/win-64/addict_yaml-0.2.5-*.tar.bz2
+conda convert --platform win-64 conda_builds/osx-64/addict_yaml-0.2.6-*.tar.bz2 -o conda_builds/
+anaconda upload conda_builds/win-64/addict_yaml-0.2.6-*.tar.bz2
 
-conda convert --platform linux-64 conda_builds/osx-64//addict_yaml-0.2.5-*.tar.bz2 -o conda_builds/
-anaconda upload conda_builds/linux-64/addict_yaml-0.2.5-*.tar.bz2
+conda convert --platform linux-64 conda_builds/osx-64//addict_yaml-0.2.6-*.tar.bz2 -o conda_builds/
+anaconda upload conda_builds/linux-64/addict_yaml-0.2.6-*.tar.bz2
--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='addict_yaml',
-    version='0.2.5',
+    version='0.2.6',
 
     description='Yaml-friendly attribute dictionaries',
     long_description=long_description,
