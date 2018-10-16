2cc75efa4be544bf0050e7104323754920e91da6
Jeff Newman <jeff@newman.me>
2018-10-16 11:58:17 -0500

    multi platform

---
+++ how_to_update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+conda build ./conda --output-folder conda_builds/ -c jpn --python 3.6
+
+conda build ./conda --output-folder conda_builds/ -c jpn --python 3.7
+
+anaconda upload conda_builds/osx-64/addict_yaml-0.2.3-*.tar.bz2
+
+conda convert --platform win-64 conda_builds/osx-64/addict_yaml-0.2.3-*.tar.bz2 -o conda_builds/
+anaconda upload conda_builds/win-64/addict_yaml-0.2.3-*.tar.bz2
+
+conda convert --platform linux-64 conda_builds/osx-64//addict_yaml-0.2.3-*.tar.bz2 -o conda_builds/
+anaconda upload conda_builds/linux-64/addict_yaml-0.2.3-*.tar.bz2
