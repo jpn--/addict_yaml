0e95b5aa6b7ab5df4e105bbefa06de67b9fece11
Jeff Newman <jeff@newman.me>
2018-10-16 10:59:01 -0500

    initial code

---
+++ addict_yaml/__init__.py
@@ -0,0 +1,93 @@
+import yaml
+import os
+import logging
+
+from addict import Dict as _Dict
+from pprint import pformat
+
+__all__ = ('yaml', 'Dict')
+
+def _prettyprint_fallback(x):
+	if isinstance(x, adict):
+		return repr(x)
+	else:
+		return pformat(x)
+
+class Dict(_Dict):
+
+	def __repr__(self):
+		if self.keys():
+			m = max(map(len, list(self.keys()))) + 1
+			return '\n'.join(['┣'+k.rjust(m) + ': ' + _prettyprint_fallback(v).replace('\n','\n┃'+' '*(m+2)) for k, v in self.items()])
+		else:
+			return self.__class__.__name__ + "()"
+
+	@classmethod
+	def load(cls, filename, *args, logger=None, **kwargs):
+		"""
+
+		Parameters
+		----------
+		filename : str
+			A filename of a yaml file to load, or a yaml file contents as a string.  
+		args
+		allow_json
+		kwargs
+
+		Returns
+		-------
+		Dict
+		"""
+		from .yaml_checker import yaml_check
+		if isinstance(filename, str) and '\n' not in filename:
+			if not os.path.exists(filename):
+				raise FileNotFoundError(filename)
+			try:
+				yaml_check(filename, logger=logging.getLogger('') if logger is None else logger)
+				with open(filename, 'r') as f:
+					return cls(yaml.load(f, *(args[1:]), **kwargs))
+			except Exception as err:
+				print("~"*40)
+				print(f"ERROR READING {filename}")
+				print("~"*40)
+				raise
+
+		else:
+			return cls(yaml.load(filename, *args, **kwargs))
+
+	@classmethod
+	def load_multi(cls, *filenames, **kwargs):
+		"""
+
+		Parameters
+		----------
+		filenames : tuple of str
+		kwargs
+
+		Returns
+		-------
+		Dict
+		"""
+		filename = filenames[0]
+		x = cls.load(filename, **kwargs)
+		for filename in filenames[1:]:
+			y = cls.load(filename, **kwargs)
+			x.update(y)
+		return x
+
+	def dump(self, *args, **kwargs):
+		from yaml import dump
+		if 'default_flow_style' not in kwargs:
+			kwargs['default_flow_style'] = False
+		if 'indent' not in kwargs:
+			kwargs['indent'] = 2
+		if len(args) and isinstance(args[0], str):
+			if os.path.exists(args[0]):
+				raise FileExistsError(args[0])
+			dirname = os.path.dirname(args[0])
+			if not os.path.exists(dirname):
+				os.makedirs(dirname)
+			with open(args[0], 'w') as f:
+				dump(self.as_plain_dict(), f, *(args[1:]), **kwargs)
+		else:
+			return dump(self.as_plain_dict(), *args, **kwargs)
---
+++ setup.py
@@ -0,0 +1,56 @@
+"""
+Yaml-friendly attribute dictionaries.
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='addict_yaml',
+    version='0.1.0',
+
+    description='Yaml-friendly attribute dictionaries',
+    long_description=long_description,
+
+    # The project's main homepage.
+    url='https://github.com/jpn--/addict_yaml',
+
+    # Author details
+    author='Jeffrey Newman',
+    author_email='jnewman@camsys.com',
+
+    # Choose your license
+    license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+    ],
+
+    # What does your project relate to?
+    keywords='addict',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+
+    install_requires=[
+        'addict>=2.2',
+        'yaml',
+    ],
+
+)
