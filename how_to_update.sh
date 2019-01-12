24070b9308797859e079b4b2317ad6074fa495b8
Jeffrey Newman <jeff@newman.me>
2019-01-11 20:06:41 -0600

    Bump version: 0.2.9 → 0.2.10

--- how_to_update.sh
+++ how_to_update.sh
@@ -4,10 +4,10 @@ conda build ./conda --output-folder conda_builds/ -c jpn --python 3.6
 
 conda build ./conda --output-folder conda_builds/ -c jpn --python 3.7
 
-anaconda upload conda_builds/osx-64/addict_yaml-0.2.9-*.tar.bz2
+anaconda upload conda_builds/osx-64/addict_yaml-0.2.10-*.tar.bz2
 
-conda convert --platform win-64 conda_builds/osx-64/addict_yaml-0.2.9-*.tar.bz2 -o conda_builds/
-anaconda upload conda_builds/win-64/addict_yaml-0.2.9-*.tar.bz2
+conda convert --platform win-64 conda_builds/osx-64/addict_yaml-0.2.10-*.tar.bz2 -o conda_builds/
+anaconda upload conda_builds/win-64/addict_yaml-0.2.10-*.tar.bz2
 
-conda convert --platform linux-64 conda_builds/osx-64//addict_yaml-0.2.9-*.tar.bz2 -o conda_builds/
-anaconda upload conda_builds/linux-64/addict_yaml-0.2.9-*.tar.bz2
+conda convert --platform linux-64 conda_builds/osx-64//addict_yaml-0.2.10-*.tar.bz2 -o conda_builds/
+anaconda upload conda_builds/linux-64/addict_yaml-0.2.10-*.tar.bz2
